ed7599b6d20e34231494b1897fc24c4a02301911
GuQiufeng <3032751404@qq.com>
2026-08-26 21:38:50 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mineral-cabinet-v20260823-todo-reconcile-fix';
+const CACHE_NAME = 'mineral-cabinet-v20260826-admin-hierarchy-fix';
 const CORE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
