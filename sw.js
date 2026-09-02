c5aaef657944667af2d57179c21c455d69617ef8
GuQiufeng <3032751404@qq.com>
2026-09-02 12:43:07 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mineral-cabinet-v20260902-garden-quartz-goethite-fix';
+const CACHE_NAME = 'mineral-cabinet-v20260902-cleanup-v3';
 const CORE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
