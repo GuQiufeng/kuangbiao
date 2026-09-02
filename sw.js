7395deec2f161836a5623f751dd6e4cacb1f9b67
GuQiufeng <3032751404@qq.com>
2026-09-02 10:51:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mineral-cabinet-v20260828-worldmap-acquisition-calendar';
+const CACHE_NAME = 'mineral-cabinet-v20260901-locality-unified';
 const CORE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
