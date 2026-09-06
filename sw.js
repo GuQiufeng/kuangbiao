00de961f93dc0f532b0111aefdfae151de20d4e1
GuQiufeng <3032751404@qq.com>
2026-09-06 18:27:13 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mineral-cabinet-v20260905-associated-minerals';
+const CACHE_NAME = 'mineral-cabinet-v20260906-basic-consistency-123';
 const CORE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
