c008aaa8f0d2484b383663274f456604a71e48d6
GuQiufeng <3032751404@qq.com>
2026-09-07 10:18:46 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mineral-cabinet-v20260906-card-native-canvas';
+const CACHE_NAME = 'mineral-cabinet-v20260906-card-native-canvas-photo-album-fix';
 const CORE = ['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
